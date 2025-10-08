050f29085ac0f4350caedec59eda4c363e4150b0
nickm <nickrmanha@gmail.com>
2025-10-07 21:42:16 -0700

    Added ShadCN and shadcn card, button, input and label components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import { CardDemo } from './components/card-demo.tsx'
 
 function App() {
   return (
     <>
-      
+      <CardDemo/>
     </>
   );
 }
