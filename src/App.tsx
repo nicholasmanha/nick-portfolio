c67236551d548ac6f9b439cda7e6190547e59be2
nickm <nickrmanha@gmail.com>
2025-10-08 22:58:42 -0700

    removed carddemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-import { CardDemo } from "./components/card-demo.tsx";
 import { Button } from "./components/ui/button.tsx";
 
 function App() {
   return (
     <>
       <div className="min-h-screen relative flex justify-center items-center">
-        <Button variant="default" >
-          SEE MORE
-
-        </Button>
-        <Button variant="outline" >
-          CODE
-
-        </Button>
+        <Button variant="default">SEE MORE</Button>
+        <Button variant="outline">CODE</Button>
       </div>
     </>
   );
