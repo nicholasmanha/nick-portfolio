dcdfb1a6518413b2b6c7ccbb98a935aeb776f5a0
nickm <nickrmanha@gmail.com>
2025-10-19 23:15:04 -0700

    add hover styles to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,13 +5,13 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
       variant: {
-        default: "bg-primary font-medium text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary font-medium text-primary-foreground hover:bg-primary/60",
         outline:
-          "border border-foreground bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border border-foreground bg-transparent shadow-xs hover:bg-black/10 dark:bg-input/30 dark:border-input dark:hover:bg-input/60",
       },
       size: {
         default: "h-9 px-5 py-2 rounded-xl has-[>svg]:px-3",
