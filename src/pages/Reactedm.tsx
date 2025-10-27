fd07288a2ad3f02268feba0583d67e868708c2f5
nickm <nickrmanha@gmail.com>
2025-10-27 09:27:23 -0700

    top card in ReactEDM project page

--- src/pages/Reactedm.tsx
+++ src/pages/Reactedm.tsx
@@ -1,9 +1,39 @@
-import ComingSoon from "@/components/ComingSoon";
+import { Button } from "@/components/ui/button";
+import Text from "@/components/ui/Text";
+import ALS from "@/assets/ALS.png";
 
 function Reactedm() {
   return (
     <>
-      <div className="flex justify-center my-16"><ComingSoon /></div>
+      <div className="flex flex-col items-center">
+        <Text variant="h1">
+          <Text variant="h1" color="primary" as="span">
+            Software Engineer{" "}
+          </Text>
+          Intern
+        </Text>
+        <Text variant="h4">Lawrence Berkeley National Lab</Text>
+        <Text>May 2017 - Dec 2025</Text>
+        <div className="flex">
+          <Button
+            link={
+              "https://blueskyproject.io/finch/?path=/docs/bluesky-components-reactedm--docs"
+            }
+            newTab
+            variant="outline"
+          >
+            DOCS
+          </Button>
+          <Button
+            link={"https://github.com/bluesky/finch"}
+            newTab
+            variant="outline"
+          >
+            CODE
+          </Button>
+        </div>
+        <img src={ALS}></img>
+      </div>
     </>
   );
 }
