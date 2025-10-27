3950c78ae462ecaea0110de6fb5d1c8ef8277801
nickm <nickrmanha@gmail.com>
2025-10-27 15:12:43 -0700

    added scroll to top component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { HashRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </StrictMode>
